9db37451fe8f4e224dc150d2f5e0d8cc15890e64
Pashi <guynatan@gmail.com>
2020-07-13 20:24:29 +0300

    new images light weight

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//
+// 	$("a").hover(function() {
+// 		console.log($(this).attr("href"));
+// 		$("head").append('<link rel="prefetch" href="' + $(this).attr("href") + '">')
+// 		/* Stuff to do when the mouse enters the element */
+// 	}, function() {
+// 		/* Stuff to do when the mouse leaves the element */
+// 	});
+//
+// });
